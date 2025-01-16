2c5dad80868c353ba26c8c22cb0af98425ba92bb
Caio Augusto <caioaugustobr0106@gmail.com>
2025-01-15 22:55:35 -0300

    criãcao do projeto

---
+++ gestao-estudantil/src/main/java/com/projeto/gestao_estudantil/GestaoEstudantilApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gestao_estudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEstudantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEstudantilApplication.class, args);
+	}
+
+}
